a01a7177ee3dc10a9f0995f14c3efd7d97b0b2a9
Cole <colekitchen56@gmail.com>
2026-08-02 16:00:55 -0400

    working home page w/o links

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,48 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+  const ranks = [
+    {name: "Gold", image: "/ranks/gold_1.png", slug: "gold"},
+    {name: "Plat", image: "/ranks/plat_1.png", slug: "plat"},
+    {name: "Diamond", image: "/ranks/diamond_1.png", slug: "diamond"},
+    {name: "Champ", image: "/ranks/champ_1.png", slug: "champ"},
+    {name: "GC", image: "/ranks/GC_1.png", slug: "gc"},
+    {name: "SSL", image: "/ranks/SSL.png", slug: "ssl"}
+  ];
+
+  const title = {name: "Title", image: "/ranks/Title.png"};
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+    <main className="bg-rl-app-bg text-white min-h-screen">
+      <div className="flex min-h-screen flex-col px-6 py-10">
+        <div className="flex justify-center mt-80 tracking-wide">
+          <Image
+            src={title.image}
+            alt={title.name}
+            width={600}
+            height={400}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
         </div>
-      </main>
-    </div>
+
+        <div className="mt-100 flex items-center">
+          <section className="grid w-full grid-cols-2 gap-10 sm:grid-cols-3 lg:grid-cols-6">
+            {ranks.map((rank) => (
+              <button
+                key={rank.name}
+                className="flex items-center justify-center aspect-square rounded-2xl border border-zinc-700 bg-zinc-800 text-lg font-semibold shadow-lg transition hover:bg-zinc-700"
+              >
+                <Image
+                  src={rank.image}
+                  alt={rank.name}
+                  width={100}
+                  height={100}
+                  />
+              </button>
+            ))}
+          </section>
+        </div>
+        </div>
+    </main>
   );
 }
